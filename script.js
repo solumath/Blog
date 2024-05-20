53e36259a1638ea28a49cb5f1447cbea31ddb889
Solumath <solumath99@gmail.com>
2024-05-21 01:44:29 +0200

    update scripts and remove css

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const left = document.getElementById("left-side");
+// after the dom is loaded
+document.addEventListener("DOMContentLoaded", function() {
+  const left = document.getElementById("left-side");
 
-const handleMove = e => {
-  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+  const handleMove = e => {
+    left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+  }
+
+  document.onmousemove = e => handleMove(e);
+
+  document.ontouchmove = e => handleMove(e.touches[0]);
 }
-
-document.onmousemove = e => handleMove(e);
-
-document.ontouchmove = e => handleMove(e.touches[0]);
+);
